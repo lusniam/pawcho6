e3d9626455c94e5cef4d0a31994c9da929c65881
lusniam <lusniamaciej1@gmail.com>
2025-04-03 12:33:13 +0200

    reset

--- index.sh
+++
@@ -1,9 +0,0 @@
-echo "\
-<html><body>\
-<h1>Aplikacja PAwChO</h1>\
-<h2>Maciej Lusnia</h2>\
-<p>Server IP: $(hostname -i)</p>\
-<p>Hostname: $(hostname)</p>\
-<p>Version: ${VERSION}</p>\
-</body></html>"\
- > /usr/share/nginx/html/index.html
